eada561feb1c0d892fcb5b5330aa0eff9d52052f
markforest <toxerius7@gmail.com>
2019-01-28 15:27:43 +0200

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $("#home-slider").owlCarousel({
         items: 1,
-        //autoplay: true,
+        autoplay: true,
         loop: true,
-        //autoplayTimeout: 10000
+        autoplayTimeout: 5000,
+        navText: ["", ""],
     });
-    document.querySelector('.owl-carousel .owl-nav button.owl-prev span').textContent = '';
-    document.querySelector('.owl-carousel .owl-nav button.owl-next span').textContent = '';
+    // document.querySelector('.owl-carousel .owl-nav button.owl-prev span').textContent = '';
+    // document.querySelector('.owl-carousel .owl-nav button.owl-next span').textContent = '';
+    $('#portfolio-carousel').owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        navText: ["<img src='images/left_sm.png'>", "<img src='images/right_sm.png'>"]
+    });
 });
